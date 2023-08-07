64be9e1f5f5923d6e974237d071bdfe7aab57211
AppsByFrancis <majer.frantisek@hotmail.com>
2023-08-07 16:57:50 +0200

    Creating File if change

--- app.js
+++ app.js
@@ -7,40 +7,66 @@ const fs = require('fs').promises;
 //Absolute Path is relative to your operating system
 
 (async () => {
+    const CREATE_FILE = "create a file";
+
     const fileHandler = await fs.open('./file.txt', 'r');
 
-    fileHandler.on( "change", async () => {
+    const createFile = async (path) => {
+        try {
 
+            const isExistingFile = await fs.open(path, "r");
+            isExistingFile.close();
+
+            return console.log(`this file ${path} already exists.`)
+
+        } catch (err) {
+
+            const newFile = await fs.open(path, "w");
+            newFile.close();
+
+            return console.log(`File: ${path} has been created.`)
+        }
+    }
+
+    fileHandler.on( "change", async () => {
+        
         console.log('file has changed')
         
         const fileSize = (await fileHandler.stat()).size;
         console.log(fileSize)
         const allocateBuffer = Buffer.alloc(fileSize); // how many bytes should be used when reading or writing files -- to not use too much space(inefficiency) / too little -> data leak
-    
+        
         const offset = 0; //99% its 0 - buffer position from which we are going to start filling file data
         const position = 0; //position from which to start reading the file, always 0, bcs we are starting from index 0;
         const length = allocateBuffer.byteLength; //number of bytes to read OR (await fileHandler.stat()).size
-    
+        
         await fileHandler.read(allocateBuffer, offset, length, position)
         //You must read the file before you convert buffer to string
         //cant convert raw buffer data to string, first must read
-
-
+        
+        
         //decoder - 01 => something meaningful
         //encoder - something meaningful => 01
         // NODE has only character encoder and decoder
+        
+        
+        if (fileHandler.includes(CREATE_FILE)){
+            const filePath = fileHandler.substring(CREATE_FILE + 1);
 
-
-
+            createFile(filePath)
+        }
+        
     } )
     // console.log(stats)
     
+
     const watcher = fs.watch('./file.txt') // watches file for changes (event.eventType = change || rename)
     
+
     
     for await (const event of watcher){
         if(event.eventType === "change"){
-
+            fileHandler.emit('change');
             
         }
     }
--- appp.js
+++ appp.js
@@ -1,35 +1,55 @@
+const { createInflate } = require('zlib');
+
 const fs = require('fs').promises;
 
-(async () => {
-    // Open file
-    const fileData = await fs.open('./file.txt', 'r');
+( async () => {
 
-    fileData.on("change", async () => {
+    const CREATE_FILE = "create a file";
+
+    const fileHandler = await fs.open('file.txt', 'r')
+
+    const createFile = async (path) => {
+        try {
+            const existingFile = await fs.open(path, 'r');
+            existingFile.close();
+            return console.log(`File: ${path} exists`)
+        } catch (err) {
+            const newFile = await fs.open(path, 'w')
+            newFile.close()
+            return console.log(`File: ${path} has been created`)
+        }
+    }
+
+    fileHandler.on("change", async () => {
         
+        //READ ARGS
+        const fileSize = (await fileHandler.stat()).size;
     
-        // Read Method args
-        const fileSize = (await fileData.stat()).size;
-    
-        const allocatedBuffer = Buffer.alloc(fileSize);
+        const buffer = Buffer.alloc(fileSize);
         const offset = 0;
-        const length = allocatedBuffer.byteLength;
+        const length = buffer.byteLength;
         const position = 0;
     
-        console.log(fileSize);
-        // Read file
-    
-        await fileData.read(allocatedBuffer, offset, length, position)
-    
-        const data = allocatedBuffer.toString('utf-8')
-        console.log(data)
+        // READ file
+        await fileHandler.read(buffer, offset, length, position);
+        console.log(buffer.toString())
+
+        if (fileHandler.includes(CREATE_FILE)){
+            const filePath = fileHandler.substring(CREATE_FILE + 1);
+            createFile(filePath)
+        }
 
     })
 
-    const watcher = fs.watch('./file.txt')
 
-    for await (const event of watcher){
-        if(event.eventType === "change"){
-            fileData.emit("change")
+
+    // WATCH file
+    const watcher = fs.watch('file.txt');
+
+    for await( const event of watcher ){
+        if(event.eventType === 'change'){
+            fileHandler.emit('change')
         }
     }
-})()
+
+})();
